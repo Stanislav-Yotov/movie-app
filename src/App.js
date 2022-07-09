b0a7a61cc4b1686cc54795a4fc3575decbbd871c
Stanislav Yotov <stanislav.iotov@abv.bg>
2022-07-09 10:54:09 +0300

    added useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -8,7 +8,7 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  async function fetchMoviesHandler() {
+  const fetchMoviesHandler = useCallback(async () => {
     try {
       setIsLoading(true);
       const response = await fetch('https://swapi.dev/api/films');
@@ -32,7 +32,12 @@ function App() {
     }
     setIsLoading(false);
 
-  };
+  }, []);
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
+
 
   let content = <p>No movies found...</p>;
 
